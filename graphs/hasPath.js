4c160b6ced40f1d27686469f08885d558b0d1e64
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-26 19:47:15 +0300

    has path, using depth-first-search iterative manner

--- graphs/hasPath.js
+++ graphs/hasPath.js
@@ -1,5 +1,5 @@
 import { graphs } from './graphs.js'
-
+/*
 export function hasPath(graph, source, destination){
        // this function figures out if there is a path from 
        // source to destination or not 
@@ -13,6 +13,28 @@ export function hasPath(graph, source, destination){
         if(current === destination) return true;
         for(let neighbor of graph[current]){
             queue.push(neighbor);
+        }
+    }
+
+    return false;
+}
+*/
+
+
+
+export function hasPath(graph, source, destination){
+    // this function figures out if there is a path between source and destination
+    // uses depth-first-search iterative approach
+
+    let stack = [ source ];
+
+    while( stack.length > 0 ){
+        let current = stack.pop();
+        
+        if(current === destination) return true;
+
+        for(let neighbor of graph[current]){
+            stack.push(neighbor);
         }
     }
 
