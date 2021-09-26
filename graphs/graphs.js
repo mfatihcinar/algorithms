b3420e0ae818e482d87cfd7e93f9cc43a02e280a
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-26 03:22:16 +0300

    sample graphs added

---
+++ graphs/graphs.js
@@ -0,0 +1,44 @@
+/* This file contains graphs to work with graph algorithms */
+
+/*
+    Graph One
+    a -> c
+    |    |
+    v    v
+    b    e
+    |
+    v
+    d <- f
+*/
+export const graphOne = {
+    a: ['b','c'],
+    b: ['d'],
+    c: ['e'],
+    d: [],
+    e: [],
+    f: ['d']
+}
+
+
+/*
+    Graph Two
+    ------> b -> c -----|
+    |                   v  
+    a----->  e  -> t -> d -> f
+    |---> g -> k
+*/
+
+export const graphTwo = {
+    a: ['b','e','g'],
+    b: ['c'],
+    c: ['d'],
+    d: ['f'],
+    e: ['t'],
+    f: [],
+    g: ['k'],
+    t: ['d'],
+    k: []
+}
+
+/* Also compile all the graphs as array */
+export const graphs = [graphOne, graphTwo];
