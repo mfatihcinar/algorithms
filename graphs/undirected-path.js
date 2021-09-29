1e8c6445568df2129285094d7a9486fbf1d28cab
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-29 13:47:22 +0300

    undirected path, using depth first search iterative approach

--- graphs/undirected-path.js
+++ graphs/undirected-path.js
@@ -1,5 +1,33 @@
 import { edgesOne } from './graphs.js'
 
+export function undirectedPath(edges, source, destination){
+    // this function returns if there is a path from source to destination
+    // in an undirected graph , implemented using Depth First Traversal using Stack
+
+    let graph = buildGraph(edges); // convert into adjacency list 
+
+    let stack = [ source ];
+
+    let visited = new Set(); // create a visited set 
+
+    while( stack.length > 0 ){
+
+        let current = stack.pop();
+
+        if( visited.has(current)) continue; // if visited, continue checking other items
+        
+        if( current === destination ) return true;
+
+        visited.add(current); // now its visited
+
+        for(let neighbor of graph[current]){
+            stack.push(neighbor);
+        }
+    }
+
+    return false;
+}
+/*
 function hasPath(graph, source, destination, visited){
 
     if(source === destination) return true;
@@ -27,9 +55,9 @@ function undirectedPath(edges, source, destination){
     
     return hasPath(graph, source, destination, new Set());
 }
+*/
 
-
-function buildGraph(edges){
+export function buildGraph(edges){
     /* this function builds a graph in adjacency list format 
        from edge list format */
     
