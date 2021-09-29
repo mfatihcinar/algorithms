be9aade085e5d0c84111b1c6bc193f52f2a14d50
trfatihcinar <42092052+fatihcinar1@users.noreply.github.com>
2021-09-29 13:44:50 +0300

    undirected path, recursive

--- graphs/undirected-path.js
+++ graphs/undirected-path.js
@@ -1,3 +1,33 @@
+import { edgesOne } from './graphs.js'
+
+function hasPath(graph, source, destination, visited){
+
+    if(source === destination) return true;
+    
+    if(visited.has(source)) return false; // if visited, signal this
+
+    visited.add(source); // now its visited
+
+    for(let neighbor of graph[source]){
+
+        if( hasPath(graph, neighbor, destination, visited) === true ){
+            return true;
+        }
+
+    }
+
+    return false;
+}
+
+function undirectedPath(edges, source, destination){
+    // this function returns if there is a path from source to destination
+    // in an undirected graph, implemented using Depth First Traversal, recursive approach
+
+    const graph = buildGraph(edges);
+    
+    return hasPath(graph, source, destination, new Set());
+}
+
 
 function buildGraph(edges){
     /* this function builds a graph in adjacency list format 
@@ -17,3 +47,7 @@ function buildGraph(edges){
 
     return graph;
 }
+
+//console.log(undirectedPath(edgesOne, 'j', 'm')); // true
+//console.log(undirectedPath(edgesOne, 'm', 'j')); // true
+console.log(undirectedPath(edgesOne, 'l', 'j')); // true
